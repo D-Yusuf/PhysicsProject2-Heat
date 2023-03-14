4e46c8dd5962fe34baa74f0378769dbb50da6898
D-Yusuf <alnasiriyusuf@gmail.com>
2023-03-14 19:59:27 +0300

    A

---
+++ app/vite.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+
+export default defineConfig({
+  build: {
+    main:resolve(__dirname, 'index.html'),
+    entry: resolve(__dirname, 'main.js'),
+  }
+})
+  
