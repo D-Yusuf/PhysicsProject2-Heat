c8acb61ad0a7ce0f97da79ca407bd960d87489d3
D-Yusuf <alnasiriyusuf@gmail.com>
2023-04-05 02:25:01 +0300

    added projectile motion project UI

---
+++ app/src/js/data.js
@@ -0,0 +1,37 @@
+export const projectileEquations = [
+    {
+        name: 'اقصى ارتفاع',
+        unit: "m",
+        symbol: 'hmax',
+        equation: 'ν∘²sin²θ/2g'
+        
+    },
+    {
+        name: 'زمن اقصى ارتفاع',
+        unit: 's',
+        symbol: 't',
+        equation: 'ν∘sinθ/g'
+        
+    },
+    {
+        name: 'المدى',
+        unit: 'm',
+        symbol: 'R',
+        equation: 'ν∘²sin2θ/g'
+        
+    },
+    {
+        name: 'السرعة الافقية',
+        unit: 'm/s',
+        symbol: 'vᕁ',
+        equation: 'ν∘cosθ'
+        
+    },
+    {
+        name: 'معادلة المسار',
+        unit: '',
+        symbol: 'y',
+        equation: 'y = tanθ·x(-g/2v°²cos²θ)x²'
+        
+    },
+]
