ff2234986a1411fb0d47f4b9e5239e1d81923033
D-Yusuf <alnasiriyusuf@gmail.com>
2023-04-08 14:55:09 +0300

    added toolboxes to sendond app, but better code
    Added them in the render() function in the projectileMotion.js file so it can be scaleable in the future 😁

--- app/src/js/projectileMotion.js
+++ app/src/js/projectileMotion.js
@@ -13,6 +13,16 @@ function render(){
     for(let equation of renderedEquations){
         renderedBoxes += `
         <div class="bg-white p-6">
+        <span class="text-info flex absolute">
+            <div
+            class="tooltip"
+            data-tip="${equation.equation}"
+            >
+            <i
+            class="fa-sharp sm:text-2xl text-xl fa-solid fa-circle-info"
+            ></i>
+            </div>
+        </span>
             <h2 class="text-xl text-title font-bold">${equation.title}</h2>
             <p class="text-info">
                 ${equation.symbol} = ${equation.equationSolution()} ${equation.unit}
